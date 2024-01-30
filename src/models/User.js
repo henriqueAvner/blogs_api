ddef0f34c9260873277ff6235b8e800c3c23c7b3
henriqueAvner <avnerhdpb@gmail.com>
2024-01-29 21:52:22 -0300

    Feat: Requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const UserModel = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: { type: DataTypes.INTEGER, primaryKey: true },
+        displayName: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING,
+        image: DataTypes.STRING,
+    },
+        {
+            timestamps: false,
+            underscored: true,
+        }
+    );
+
+    return User;
+}
+
+module.exports = UserModel;
