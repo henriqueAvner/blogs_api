190cca15d45aa30aef58e114d4b8919723f65c6d
henriqueAvner <avnerhdpb@gmail.com>
2024-01-30 17:37:33 -0300

    Fixed: Requisito 3 - retirando a importação

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { userController } = require('./controllers');
-const { validateToken } = require('./middlewares/auth.middleware');
 
 // ...
 
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,7 +5,6 @@ const loginController = async (req, res) => {
   const { email, password } = req.body;
 
   const { status, data } = await userService.serviceLogin(email, password);
-  console.log(data);
   return res.status(httpMapCode[status]).json(data);
 };
 
